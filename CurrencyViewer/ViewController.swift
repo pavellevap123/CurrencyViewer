460c38a7736a00b66c70e101549bb25af5d1f83a
Pavel Paddubotski <52162983+pavellevap123@users.noreply.github.com>
2020-07-07 09:15:58 +0300

    Initial Commit

---
+++ CurrencyViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyViewer
+//
+//  Created by Pavel on 7/7/20.
+//  Copyright © 2020 Pavel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
